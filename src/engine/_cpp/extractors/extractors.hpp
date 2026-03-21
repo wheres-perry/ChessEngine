8dad29a1f9a9d334b0a532e61ed5ea08007da612
Ethan Perry <86326912+wheres-perry@users.noreply.github.com>
2026-03-21 03:14:44 -0600

    style: fix C++ code formatting to pass clang-format CI check

--- src/engine/_cpp/extractors/extractors.hpp
+++ src/engine/_cpp/extractors/extractors.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <vector>
 #include "../board/board.hpp"
+#include <vector>
 
 namespace extractors {
 
@@ -10,19 +10,19 @@ namespace extractors {
 // Channels 0-11: Pieces (White P-K, Black P-K)
 // Channel 12: Side to Move (1 if White, 0 if Black)
 // Channels 13-16: Castling rights (WK, WQ, BK, BQ)
-std::vector<float> extract_cnn(const Board& board);
+std::vector<float> extract_cnn(const Board &board);
 
 // B. NNUE Sparse Categorical Extractor (HalfKP)
 // Returns a vector of active feature indices for the Side to Move.
-std::vector<int> extract_halfkp(const Board& board);
+std::vector<int> extract_halfkp(const Board &board);
 
 // D. GNN Graph Extractor
 struct GraphData {
-    // Flat array of [num_pieces * 3], each chunk is (square, piece_type, color)
-    std::vector<int> nodes; 
-    // Flat array of [2 * num_edges], each chunk is (src_node_idx, dst_node_idx)
-    std::vector<int> edges; 
+  // Flat array of [num_pieces * 3], each chunk is (square, piece_type, color)
+  std::vector<int> nodes;
+  // Flat array of [2 * num_edges], each chunk is (src_node_idx, dst_node_idx)
+  std::vector<int> edges;
 };
-GraphData extract_gnn(const Board& board);
+GraphData extract_gnn(const Board &board);
 
 } // namespace extractors
